e82d5f9c9264c449eb4c0bbb5e541a71af1e763c
Alonzo Green <alonzo@brainspin.com>
2018-11-08 15:36:00 -0700

    created form and some javascript to start with

---
+++ gmail_api.js
@@ -0,0 +1,50 @@
+let gmail_API_URL = "https://www.googleapis.com/gmail/v1/users";
+
+/**
+ * Retrieve Messages in user's mailbox matching query.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {String} query String used to filter the Messages listed.
+ * @param  {Function} callback Function to call when the request is complete.
+ */
+function listMessages(userId, query, callback) {
+  var getPageOfMessages = function(request, result) {
+    request.execute(function(resp) {
+      result = result.concat(resp.messages);
+      var nextPageToken = resp.nextPageToken;
+      if (nextPageToken) {
+        request = gapi.client.gmail.users.messages.list({
+          'userId': userId,
+          'pageToken': nextPageToken,
+          'q': query
+        });
+        getPageOfMessages(request, result);
+      } else {
+        callback(result);
+      }
+    });
+  };
+  var initialRequest = gapi.client.gmail.users.messages.list({
+    'userId': userId,
+    'q': query
+  });
+  getPageOfMessages(initialRequest, []);
+}
+
+/**
+ * Get all the Labels in the user's mailbox.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {Function} callback Function to call when the request is complete.
+ */
+function listLabels(userId, callback) {
+  var request = gapi.client.gmail.users.labels.list({
+    'userId': userId
+  });
+  request.execute(function(resp) {
+    var labels = resp.labels;
+    callback(labels);
+  });
+}
