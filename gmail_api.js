a15528b2c94f82305535671145c717917ab8ca3b
Alonzo Green <alonzo@brainspin.com>
2018-11-09 10:11:54 -0700

    Updated index page to match google's test page to authorize API

--- gmail_api.js
+++ gmail_api.js
@@ -1,50 +1,111 @@
-let gmail_API_URL = "https://www.googleapis.com/gmail/v1/users";
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '769177896397-edpv4hv24pa90g30q811vl62h2albhsa.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDbUIiSuoQ0_xI7ZsF17ZvK2zEQLMD7jaE';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+var visibleWhenAuthorized = document.getElementsByClassName("visible-when-authoized");
 
 /**
- * Retrieve Messages in user's mailbox matching query.
- *
- * @param  {String} userId User's email address. The special value 'me'
- * can be used to indicate the authenticated user.
- * @param  {String} query String used to filter the Messages listed.
- * @param  {Function} callback Function to call when the request is complete.
+ *  On load, called to load the auth2 library and API client library.
  */
-function listMessages(userId, query, callback) {
-  var getPageOfMessages = function(request, result) {
-    request.execute(function(resp) {
-      result = result.concat(resp.messages);
-      var nextPageToken = resp.nextPageToken;
-      if (nextPageToken) {
-        request = gapi.client.gmail.users.messages.list({
-          'userId': userId,
-          'pageToken': nextPageToken,
-          'q': query
-        });
-        getPageOfMessages(request, result);
-      } else {
-        callback(result);
-      }
-    });
-  };
-  var initialRequest = gapi.client.gmail.users.messages.list({
-    'userId': userId,
-    'q': query
-  });
-  getPageOfMessages(initialRequest, []);
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
 }
 
 /**
- * Get all the Labels in the user's mailbox.
- *
- * @param  {String} userId User's email address. The special value 'me'
- * can be used to indicate the authenticated user.
- * @param  {Function} callback Function to call when the request is complete.
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
  */
-function listLabels(userId, callback) {
-  var request = gapi.client.gmail.users.labels.list({
-    'userId': userId
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
   });
-  request.execute(function(resp) {
-    var labels = resp.labels;
-    callback(labels);
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    //signoutButton.style.display = 'block';
+    for(var i = 0; i < visibleWhenAuthorized.length; i++){
+        visibleWhenAuthorized[i].style.display = visibleWhenAuthorized[i].dataset.visible;
+    }
+    listLabels();
+  } else {
+    authorizeButton.style.display = 'block';
+    //signoutButton.style.display = 'none';
+    for(var i = 0; i < visibleWhenAuthorized.length; i++){
+        visibleWhenAuthorized[i].style.display = "none";
+    }
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+/**
+ * Print all Labels in the authorized user's inbox. If no labels
+ * are found an appropriate message is printed.
+ */
+function listLabels() {
+  gapi.client.gmail.users.labels.list({
+    'userId': 'me'
+  }).then(function(response) {
+    var labels = response.result.labels;
+    appendPre('Labels:');
+
+    if (labels && labels.length > 0) {
+      for (i = 0; i < labels.length; i++) {
+        var label = labels[i];
+        appendPre(label.name)
+      }
+    } else {
+      appendPre('No Labels found.');
+    }
   });
 }
